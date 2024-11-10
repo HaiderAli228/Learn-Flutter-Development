0f8be1ec1233544943d81e69e6183b220421c897
HaiderAli228 <flutter2830@gmail.com>
2024-11-10 16:38:37 +0500

    adding animation

---
+++ lib/animations/lottie_animation.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class LottieAnimation extends StatefulWidget {
+  const LottieAnimation({super.key});
+
+  @override
+  State<LottieAnimation> createState() => _LottieAnimationState();
+}
+
+class _LottieAnimationState extends State<LottieAnimation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
