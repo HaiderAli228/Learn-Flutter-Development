2b6899afc04bccf74789ada521c95101195940d9
HaiderAli228 <flutter2830@gmail.com>
2025-01-27 11:49:30 +0500

    adding and revising the basic

---
+++ lib/Widgets/basic_widgets.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class BasicWidgets extends StatefulWidget {
+  const BasicWidgets({super.key});
+
+  @override
+  State<BasicWidgets> createState() => _BasicWidgetsState();
+}
+
+class _BasicWidgetsState extends State<BasicWidgets> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        centerTitle: true,
+        title: Text(
+          "Basic Widgets",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
